de8e7a69b9eef68b84907e9a1b37149724f74f79
sts10 <sschlinkert@gmail.com>
2019-12-01 16:14:27 -0500

    makes main semi-helpul

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hi there!");
+    println!("The code for each day's challenge (1 through, hopefully-but-probably-not-all-the-way-to, 25) is in a Rust executable in src/bin. Thus the code for, say, Day 2's executable is located in src/bin/day02.rs.");
+
+    println!("\n\nTo run the Day 2 executable, from the root directory run 'cargo run --bin day02'. To run tests, if there are any, run 'cargo test --bin day02'.");
 }

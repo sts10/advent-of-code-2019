d969ede325eba8a2efedca41acffe1e7956d5c24
sts10 <sschlinkert@gmail.com>
2019-12-01 20:11:24 -0500

    a nice refactoring of my day 1 solution, thanks to some help from the internet and new friends

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -5,36 +5,38 @@ fn main() {
     let mut total_fuel_part_1 = 0;
     let mut total_fuel_part_2 = 0;
     for mass in masses {
-        total_fuel_part_1 += get_fuel_from_mass(mass);
+        total_fuel_part_1 += get_fuel_from_mass(mass).unwrap_or(0);
         total_fuel_part_2 += get_fuel_from_mass_part_2(mass);
     }
     println!("Part 1 total fuel: {}", total_fuel_part_1);
     println!("Part 2 total fuel: {}", total_fuel_part_2);
 }
 
-fn get_fuel_from_mass(mass: usize) -> isize {
+fn get_fuel_from_mass(mass: usize) -> Option<usize> {
     // take its mass, divide by three, round down, and subtract 2.
-    ((mass as f64 / 3.0).floor() as isize) - 2
+    ((mass as f64 / 3.0).floor() as usize).checked_sub(2)
 }
 
 fn get_fuel_from_mass_part_2(mass: usize) -> usize {
     let mut total_fuel: usize = 0;
-    let mut new_mass: isize = mass as isize;
-    loop {
-        let this_run: isize = get_fuel_from_mass(new_mass as usize);
-        if this_run < 0 {
-            break;
-        }
-        total_fuel += this_run as usize;
-        new_mass = this_run;
+    let mut current_mass: usize = mass;
+    while current_mass > 0 {
+        match get_fuel_from_mass(current_mass) {
+            Some(fuel) => {
+                total_fuel += fuel;
+                current_mass = fuel as usize;
+                fuel
+            }
+            None => break,
+        };
     }
     total_fuel
 }
 
 #[test]
 fn can_find_fuel_given_mass() {
-    assert_eq!(get_fuel_from_mass(12), 2);
-    assert_eq!(get_fuel_from_mass(100756), 33583);
+    assert_eq!(get_fuel_from_mass(12), Some(2));
+    assert_eq!(get_fuel_from_mass(100756), Some(33583));
 }
 
 #[test]

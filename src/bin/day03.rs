c43f68671c23e09500f1eb385a2f07f3be0cf654
sts10 <sschlinkert@gmail.com>
2019-12-03 23:08:48 -0500

    stakc overflow

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -3,55 +3,71 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-    let wire1 = vec!["R8", "U5", "L5", "D3"];
-    let wire2 = vec!["U7", "R6", "D4", "L4"];
-    let mut panel: [[usize; 30]; 30] = [[0; 30]; 30];
-    panel = place_wire(wire1, panel);
+    // let wire1 = vec!["R8", "U5", "L5", "D3"];
+    // let wire2 = vec!["U7", "R6", "D4", "L4"];
+    let wire1 = vec!["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"];
+    let wire2 = vec!["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"];
+    let mut panel: [[usize; 1500]; 1500] = [[0; 1500]; 1500];
+    panel = place_wire(wire1, 1, panel);
+    panel = place_wire(wire2, 8, panel);
+
     // println!("Panel is now {:?}", panel);
-    for row in panel.iter() {
-        println!("{:?}", row);
-    }
+    // for row in panel.iter() {
+    //     println!("{:?}", row);
+    // }
+    let min_cp = find_min_cross_point_manhattan_distance(panel, 9);
+    println!("Found min cross point to be {}", min_cp);
 }
 
-fn place_wire(wire: Vec<&str>, mut panel: [[usize; 30]; 30]) -> [[usize; 30]; 30] {
-    let arbitrary_central_point = (12, 8);
+fn place_wire(
+    wire: Vec<&str>,
+    number_to_place: usize,
+    mut panel: [[usize; 1500]; 1500],
+) -> [[usize; 1500]; 1500] {
+    let arbitrary_central_point = (8, 1);
     let mut new_starting_point = arbitrary_central_point;
     for run in wire {
         // this is giving the error: "invalid left-hand expression"
-        (panel, new_starting_point) = place_run(run, &panel, arbitrary_central_point);
+        let (generated_panel, generated_new_starting_point) =
+            place_run(run, number_to_place, panel, new_starting_point);
+        panel = generated_panel;
+        new_starting_point = generated_new_starting_point;
     }
+    panel[new_starting_point.0][new_starting_point.1] = number_to_place;
     panel
 }
 fn place_run(
     run: &str,
-    panel: &[[usize; 30]; 30],
+    number_to_place: usize,
+    panel: [[usize; 1500]; 1500],
     starting_point: (usize, usize),
-) -> ([[usize; 30]; 30], (usize, usize)) {
+) -> ([[usize; 1500]; 1500], (usize, usize)) {
     let (direction, amount) = get_direction_and_amount(run);
     let mut new_panel = panel.clone();
-    let mut new_current_point: (usize, usize);
+    let mut new_current_point: (usize, usize) = starting_point;
     match direction {
         'R' => {
-            for n in 1..amount {
-                new_panel[starting_point.0][starting_point.1 + n] = 1;
+            for n in 0..amount {
+                new_panel[starting_point.0][starting_point.1 + n] += number_to_place;
             }
             new_current_point = (starting_point.0, starting_point.1 + amount);
         }
         'L' => {
-            for n in 1..amount {
-                new_panel[starting_point.0][starting_point.1 - n] = 1;
+            for n in 0..amount {
+                // try 3 dots?
+                new_panel[starting_point.0][starting_point.1 - n] += number_to_place;
             }
             new_current_point = (starting_point.0, starting_point.1 - amount);
         }
         'U' => {
-            for n in 1..amount {
-                new_panel[starting_point.0 - n][starting_point.1] = 1;
+            for n in 0..amount {
+                new_panel[starting_point.0 - n][starting_point.1] += number_to_place;
             }
             new_current_point = (starting_point.0 - amount, starting_point.1);
         }
         'D' => {
-            for n in 1..amount {
-                new_panel[starting_point.0 + n][starting_point.1] = 1;
+            for n in 0..amount {
+                new_panel[starting_point.0 + n][starting_point.1] += number_to_place;
                 new_current_point = (starting_point.0 + amount, starting_point.1);
             }
         }
@@ -67,6 +83,48 @@ fn get_direction_and_amount(run: &str) -> (char, usize) {
     let amount: Vec<String> = amount.into_iter().map(|c| c.to_string()).collect();
     let amount = amount.to_vec().join("").parse::<usize>().unwrap();
     (direction, amount)
+}
+
+fn find_min_cross_point_manhattan_distance(
+    panel: [[usize; 1500]; 1500],
+    value_to_hunt_for: usize,
+) -> isize {
+    let cross_points = find_all_cross_points(panel, value_to_hunt_for);
+    let mut min_cross_point_manhattan_distance = 100000;
+    for cross_point in cross_points {
+        let this_cross_point_manhattan_distance = get_manhattan_distance((8, 1), cross_point);
+        if this_cross_point_manhattan_distance == 0 {
+            continue;
+        }
+        if this_cross_point_manhattan_distance < min_cross_point_manhattan_distance {
+            min_cross_point_manhattan_distance = this_cross_point_manhattan_distance;
+        }
+    }
+    min_cross_point_manhattan_distance
+}
+
+fn find_all_cross_points(
+    panel: [[usize; 1500]; 1500],
+    value_to_hunt_for: usize,
+) -> Vec<(usize, usize)> {
+    let mut cross_points: Vec<(usize, usize)> = vec![];
+    let mut row_number = 0;
+    for row in panel.iter() {
+        let mut column_number = 0;
+        for square in row.into_iter() {
+            if panel[row_number][column_number] == value_to_hunt_for {
+                cross_points.push((row_number, column_number));
+            }
+            column_number += 1;
+        }
+        row_number += 1;
+    }
+    cross_points
+}
+fn get_manhattan_distance(a: (usize, usize), b: (usize, usize)) -> isize {
+    // isize::abs((a.0 - b.0) as isize) + isize::abs((a.1 - b.1) as isize)
+    isize::abs((a.0 as isize - b.0 as isize) as isize)
+        + isize::abs((a.1 as isize - b.1 as isize) as isize)
 }
 
 fn read_string_from_file(file_path: &str) -> io::Result<String> {

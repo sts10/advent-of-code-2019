0042c6f53a6d8c0f593d46acaba04254631da851
sts10 <sschlinkert@gmail.com>
2019-12-04 23:53:18 -0500

    passes examples, but can't get the answer

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -4,33 +4,41 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-    // let wire1 = vec!["R8", "U5", "L5", "D3"]
+    // let wire1: Vec<String> = vec!["R8", "U5", "L5", "D3"]
     //     .iter()
     //     .map(|s| s.to_string())
     //     .collect();
-    // let wire2 = vec!["U7", "R6", "D4", "L4"]
+    // let wire2: Vec<String> = vec!["U7", "R6", "D4", "L4"]
     //     .iter()
     //     .map(|s| s.to_string())
     //     .collect();
-    // let wire1 = vec!["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"];
-    // let wire2 = vec!["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"];
-    let wire1 = vec![
-        "R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51",
-    ]
-    .iter()
-    .map(|s| s.to_string())
-    .collect();
-    let wire2 = vec![
-        "U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7",
-    ]
-    .iter()
-    .map(|s| s.to_string())
-    .collect();
 
-    // let file_name = "inputs/day03.txt";
-    // let wires_as_vec: Vec<String> = read_by_line(file_name).unwrap();
-    // let wire1 = split_cs_string(wires_as_vec[0].clone());
-    // let wire2 = split_cs_string(wires_as_vec[1].clone());
+    // let wire1 = vec!["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
+    //     .iter()
+    //     .map(|s| s.to_string())
+    //     .collect();
+    // let wire2 = vec!["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
+    //     .iter()
+    //     .map(|s| s.to_string())
+    //     .collect();
+
+    // let wire1 = vec![
+    //     "R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51",
+    // ]
+    // .iter()
+    // .map(|s| s.to_string())
+    // .collect();
+    // let wire2 = vec![
+    //     "U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7",
+    // ]
+    // .iter()
+    // .map(|s| s.to_string())
+    // .collect();
+
+    let file_name = "inputs/day03.txt";
+    let wires_as_vec: Vec<String> = read_by_line(file_name).unwrap();
+    let wire1 = split_cs_string(wires_as_vec[0].clone());
+    let wire2 = split_cs_string(wires_as_vec[1].clone());
 
     let wire1_as_coordinates = make_coordinates(wire1);
     let wire2_as_coordinates = make_coordinates(wire2);
@@ -42,7 +50,7 @@ fn main() {
 
     let part_2_solution = solve_part_2(wire1_as_coordinates, wire2_as_coordinates);
     println!("part 2 solution is: {}", part_2_solution);
-    println!("12338 is too high!");
+    println!("12338 is too high, and 12307 isn't correct either!");
 }
 
 fn make_coordinates(instructions: Vec<String>) -> Vec<(isize, isize)> {
